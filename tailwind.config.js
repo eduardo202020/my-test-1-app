d353d3584a44837f7ef327e0e4b8530af840b8dc
Eduardo26 <jguevaral@uni.pe>
2023-06-11 09:21:33 -0500

    Set up project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./**/*.{js,jsx,ts,tsx}",
+    "./screens/HomeScreen.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
